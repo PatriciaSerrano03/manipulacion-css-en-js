b36c23875c4752cd8d742e9333f065bf868766ca
PatriiSerrano <patricia.serranomolina@gmail.com>
2024-06-26 15:42:01 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const boxElement = document.getElementById('box');
+
+const ButtonElement = document.getElementById('button');
+
+ButtonElement.addEventListener('click', () => {
+  if (boxElement.classList.contains('box--green')) {
+    boxElement.classList.remove('box-green');
+  } else {
+    boxElement.classList.add('box-green');
+  }
+});
+
+const formElement = document.getElementById('form');
+
+const checkForm = event => {
+  event.preventDefault();
+};
+
+formElement.addEventListener('submit', checkForm);
+
+formElement.addEventListener('input', event => {
+  console.log(event.target.value);
+});
